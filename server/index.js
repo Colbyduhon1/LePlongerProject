2f7ee006954b868559d9ad3ebf6f26734a0fb499
Jackson Carter <jcarter0803@gmail.com>
2017-05-03 15:36:03 -0700

    added middlewear

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
-
 const app = express();
-
 const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', function(req, res){
+  res.sendStatus(200);
+});
+
+
+
 
 app.listen(port, function () {
 	console.log('server running on port ', port);
