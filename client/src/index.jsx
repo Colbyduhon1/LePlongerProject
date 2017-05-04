34035421c268fc93e21812520be77f5949371a3f
Aly-Tamboura <alytamboura7370@gmail.com>
2017-05-04 10:27:50 -0700

    added bootstrap page setup

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -14,42 +14,48 @@ class App extends React.Component {
   render () {
     return (
       <div className='container'>
-
         <div className='row'>
+
+         {/*transfer to search component*/}
           <div className='col-md-12'>
             <h1>search component</h1>
           </div>
-        </div>
+
+        </div>{/* end first row */}
 
         <div className='row'>
 
           <div className='col-md-3'>
-            <div className='col-md-12'>
-             <h1>norcal</h1>
+
+            {/* transfer to top ocean component */}
+            <div className='col-md-12 weather-section' >
+             <h1>norcal ocean map</h1>
             </div>
 
-            <div className='col-md-12'>
-             <h1>centeral</h1>
+            {/* transfer to top ocean component */}
+            <div className='col-md-12 weather-section'>
+             <h1>centeral ocean map</h1>
             </div>
-            <div className='col-md-12'>
-             <h1>sorcal</h1>
+
+            {/* transfer to top ocean component */}
+            <div className='col-md-12 weather-section'>
+             <h1>socal ocean map</h1>
             </div>
+
+          </div>{/* end left col */}
+
+          {/* transfer to map component */}
+          <div className='col-md-6 map-section'>
+            <h1>map section</h1>
           </div>
 
-          <div className='col-md-6'>
-            <h1>weather section</h1>
-          </div>
-
-          <div className='col-md-3'>
-            <h1>weather section</h1>
+          {/* transfer to reviews component */}
+          <div className='col-md-3 reviews-section'>
+            <h1>reviews section</h1>
+            <button className='btn btn-danger'>submit</button>
           </div>
 
         </div>
-
-
-
-
-
       </div>
     )
   }
