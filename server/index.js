a653e298f0fbc666cd8fffe95ee18c538a8523d6
Aly-Tamboura <alytamboura7370@gmail.com>
2017-05-03 18:28:23 -0700

    test

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+var path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
