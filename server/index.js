966421b896d94e88662b664510e82dcf92fd442b
Aly-Tamboura <alytamboura7370@gmail.com>
2017-05-03 18:25:04 -0700

    test

--- server/index.js
+++ server/index.js
@@ -9,11 +9,11 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '../client/dist'));
+app.use(express.static(path.join(__dirname, '../client/dist/')));
+
 
 app.get('/', function(req, res) {
-  res.sendFile(express.static(__dirname + "../client/dist/index.html"));
-  //res.end('hello world');
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 })
 
 
