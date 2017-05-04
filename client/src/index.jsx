404487834fe5ee23725e3ba1746ab283066f97bf
Aly-Tamboura <alytamboura7370@gmail.com>
2017-05-04 09:01:55 -0700

    added bootstrap

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -13,8 +13,43 @@ class App extends React.Component {
 
   render () {
     return (
-      <div>
-      <h1> hfkdkdjdkfj</h1>
+      <div className='container'>
+
+        <div className='row'>
+          <div className='col-md-12'>
+            <h1>search component</h1>
+          </div>
+        </div>
+
+        <div className='row'>
+
+          <div className='col-md-3'>
+            <div className='col-md-12'>
+             <h1>norcal</h1>
+            </div>
+
+            <div className='col-md-12'>
+             <h1>centeral</h1>
+            </div>
+            <div className='col-md-12'>
+             <h1>sorcal</h1>
+            </div>
+          </div>
+
+          <div className='col-md-6'>
+            <h1>weather section</h1>
+          </div>
+
+          <div className='col-md-3'>
+            <h1>weather section</h1>
+          </div>
+
+        </div>
+
+
+
+
+
       </div>
     )
   }
