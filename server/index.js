1e4145b50addfffb57c108e77d57b9a5f0082768
Aly-Tamboura <alytamboura7370@gmail.com>
2017-05-03 18:13:27 -0700

    test

--- server/index.js
+++ server/index.js
@@ -4,14 +4,16 @@ const app = express();
 const port = process.env.PORT || 8080;
 const path = require('path');
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(__dirname + '../client/dist'));
+
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', function(req, res) {
-  //res.sendFile(path.join(__dirname, "../client/dist/"));
-  res.end('hello world');
+  res.sendFile(path.join(__dirname, "../client/dist/index"));
+  //res.end('hello world');
 })
 
 
