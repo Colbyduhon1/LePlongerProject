356637dd810e6cd1c9c773848e310decd1addb01
Aly-Tamboura <alytamboura7370@gmail.com>
2017-05-03 17:57:43 -0700

    added route for index.html

--- server/index.js
+++ server/index.js
@@ -2,15 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8080;
+const path = require('path');
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('/', function(req, res){
-  res.sendFile(express.static(__dirname + '/../client/dist/index.html'));
-});
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+})
 
 
 
