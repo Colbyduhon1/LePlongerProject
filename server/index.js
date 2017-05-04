6b1caa3ae20a2fb6be5b6fab5d80744a30e5da00
Aly-Tamboura <alytamboura7370@gmail.com>
2017-05-03 18:06:28 -0700

    test

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist"));
 })
 
 
