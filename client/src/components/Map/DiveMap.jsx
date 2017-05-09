46a1da8b9a69c2480a5e231a7e439161da188f55
jiaconis <john.t.iaconis@gmail.com>
2017-05-08 18:18:51 -0700

    updating for merge

--- client/src/components/Map/DiveMap.jsx
+++ client/src/components/Map/DiveMap.jsx
@@ -4,6 +4,7 @@ import {withGoogleMap, GoogleMap, Marker, InfoWindow} from 'react-google-maps';
 import MarkerContainer from './MarkerContainer.jsx';
 
 const DiveMap = withGoogleMap(props => (
+<<<<<<< HEAD
  <GoogleMap
    ref={props.onMapLoad}
    defaultZoom={14}
@@ -14,6 +15,19 @@ const DiveMap = withGoogleMap(props => (
        <MarkerContainer marker={marker} key={index} />
    ))}
    </GoogleMap>
+=======
+  <GoogleMap
+    ref={props.onMapLoad}
+    defaultZoom={14}
+    defaultCenter={{lat: 37.787, lng: -122.458}}
+    onClick={props.onMapClick}
+  >
+    {props.markers.map( (marker, index) => (
+        <MarkerContainer marker={marker} key={index} />
+    ))}
+
+    </GoogleMap>
+>>>>>>> updating for merge
 ));
 
 export default DiveMap;
