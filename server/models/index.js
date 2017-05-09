1fe50eb7612cee2d0b4bbf16091f2293559cb7ab
Nobodyz710 <colbyduhon@Colbys-MacBook-Air.local>
2017-05-09 16:53:12 -0700

    Add comment sample data, posting comment method for worker and router

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+
 const connection = require('../db');
 const request = require('request');
 const Api = require('../../keys');
@@ -15,18 +15,11 @@ module.exports = {
       //NEED TO FIGURE OUT HOW WE ARE CALLING THIS AITH AUTHENITCATION
       );
     },
-    post: (new_user, callback) => {
-      var user = [new_user.id, new_user.name, new_user.password, new_user.email, new_user.salt, new_user.age, new_user.skill];
-      var queryString = 'INSERT INTO users(id, name, password, email, salt, age, skill ) VALUES (?, ?, ?, ?, ?, ?, ?)';
-      connection.query(queryString, user, function(err, data) {
-        if (err) {
-          console.log('could not post user to database');
-          callback(err, null);
-        } else {
-          console.log('posted users to database');
-          callback(null, data);
-        }
-      })
+    post: (new_user) => {
+      return connection.queryAsync(
+        `INSERT INTO users (Name, password, salt) VALUES ('${new_user.name}', '${new_user.password}',
+        '${new_user.salt}');`
+      );
     }
   },
   dive_sites: {
@@ -54,23 +47,34 @@ module.exports = {
       return connection.queryAsync(
         `SELECT * FROM comments;`
       );
+    },
+    post: (new_comment, callback) => {
+      var NewComment = [new_comment.id, new_comment.diveSite_id, new_comment.message, new_comment.user_id];
+      var queryString = 'INSERT INTO comments(id, diveSite_id, message, user_id) VALUES(?,?,?,?)'
+      connection.query(queryString, NewComment ,function(err, data){
+        if (err){
+          console.log('could not post comment to database');
+          callback(err, null);
+        }
+        else{
+          console.log('posted new comment data to database');
+          callback(null,data)
+        }
+      })
     }
   },
   weather: {
-    //uncomment url for actual use, disabled so we don't hit api limit
-    get: (req, res) => {
-      const location = `${req.body.location.lat},${req.body.location.lng}`
-      // const url = `http://api.wunderground.com/api/${Api.weatherUnderground}/geolookup/conditions/q/${location}.json`
-      console.log('url: ', url);
-
-      axios.get(url)
-        .then( (result) => {
-          console.log('received data from weatherUnderground');
-          res.json(result.data);
-        })
-        .catch( (err) => {
-          console.log('error from weather api: ', err.message);
-        })
+    get: (callback) => {
+      request('http://api.wunderground.com/api/' + Api.weatherUnderground  + '/geolookup/conditions/q/36.52167,-121.95361.json', function (err, response, body) {
+        if (err) {
+          console.log('err:', err);
+          callback(err, null)
+        } else {
+          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+          console.log(body)
+          callback(null, body)
+        }
+      });
     }
   }
 };
