c6ed79e21e0f4870e4d5d1d91d5fd93de6c17ff2
jiaconis <john.t.iaconis@gmail.com>
2017-05-10 17:13:44 -0700

    Set up route for gathering ocean info

--- server/models/index.js
+++ server/models/index.js
@@ -91,7 +91,7 @@ module.exports = {
  //uncomment url for actual use, disabled so we don't hit api limit
     get: (req, res) => {
       const location = `${req.body.location.lat},${req.body.location.lng}`
-      const url = `http://api.wunderground.com/api/${Api.weatherUnderground}/geolookup/conditions/q/${location}.json`
+      // const url = `http://api.wunderground.com/api/${Api.weatherUnderground}/geolookup/conditions/q/${location}.json`
 
       axios.get(url)
         .then( (result) => {
@@ -127,6 +127,12 @@ module.exports = {
         })
         //remove this when using real api
         res.send(200);
+    }
+  },
+  ocean: {
+    get: (req, res) => {
+      console.log('hello');
+      res.send('world');      
     }
   }
 };
