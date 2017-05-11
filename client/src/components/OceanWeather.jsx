a5124c29e31c55d78501b96e9d2271d75eb99511
jiaconis <john.t.iaconis@gmail.com>
2017-05-11 16:36:03 -0700

    for merge

--- client/src/components/OceanWeather.jsx
+++ client/src/components/OceanWeather.jsx
@@ -7,38 +7,25 @@ class OceanWeather extends React.Component {
     super(props);
     // this.findCurrentMaxHeight = this.findCurrentMaxHeight.bind(this);
     this.state = {
-      waveheight: this.props.graphHeight + 1
+      waveheight: this.props.graphHeight
     }
   }
-
-  // componentDidMount() {
-  //   console.log('test');
-  //   this.props.data[0].forEach( (point) => {
-  //     console.log(point);
-  //     if (point.y > this.state.waveh) {
-  //       console.log('setting max to: ', point.y);
-  //       this.setState({
-  //         waveheight: point.y
-  //       })
-  //     }
-  //   });
-  // }
-
 
   render() {
      return(
       <div className='col-md-12 weather-section'>
         <p>Wave Height for the Past 12 Hours</p>
         <AreaChart
-        axes
-        grid
-        noAreaGradient
-        width={400}
-        height={200}
-        areaColors={['blue']}
-        yDomainRange={[0, this.state.waveheight]}
-        interpolate={'cardinal'}
-        data={this.props.data}/>
+          axes
+          grid
+          noAreaGradient
+          width={400}
+          height={200}
+          areaColors={['blue']}
+          yDomainRange={[0, this.state.waveheight]}
+          interpolate={'cardinal'}
+          data={this.props.data}
+        />
       </div>
     );
   }
