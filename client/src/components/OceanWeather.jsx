294058f76c0007b2ce221e8ce27cadb37cdcc1a3
jiaconis <john.t.iaconis@gmail.com>
2017-05-11 11:15:44 -0700

    Added a "chart" to ocean weather section

--- client/src/components/OceanWeather.jsx
+++ client/src/components/OceanWeather.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import $ from 'jquery';
+import {AreaChart} from 'react-easy-chart';
 
 class OceanWeather extends React.Component {
   constructor(props) {
@@ -16,9 +17,20 @@ class OceanWeather extends React.Component {
   }
 
   render() {
-    return(
+     return(
       <div className='col-md-12 weather-section'>
         <p>Lots of Ocean info</p>
+        <AreaChart  data={[
+      [
+        { x: 1, y: 20 },
+        { x: 2, y: 10 },
+        { x: 3, y: 25 }
+      ], [
+        { x: 1, y: 10 },
+        { x: 2, y: 12 },
+        { x: 3, y: 4 }
+      ]
+    ]}/>
       </div>
     );
   }
