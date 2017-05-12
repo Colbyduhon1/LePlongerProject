5b143eb1d3b7544a85aabf732c23586b518ebfa4
jiaconis <john.t.iaconis@gmail.com>
2017-05-11 21:07:20 -0700

    Fixed querystring

--- server/models/index.js
+++ server/models/index.js
@@ -68,7 +68,7 @@ module.exports = {
   comments: {
     get: (req,res) => {
       let diveID = req.body.diveSite_id;
-      let queryString = 'SELECT * FROM comments INNER JOIN dives ON dives.id=comments.divesite_id WHERE comments.divesite_id=' + diveID;
+      let queryString = 'SELECT * FROM comments INNER JOIN dives ON dives.id=comments.divesite_id LEFT JOIN users ut on comments.user_id = ut.id WHERE comments.divesite_id=' + diveID;
       return connection.queryAsync(
         queryString
       );
