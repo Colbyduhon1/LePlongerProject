e0a7668d490d7af7a25ad9053bca6a61fd5bd835
jiaconis <john.t.iaconis@gmail.com>
2017-05-13 15:02:23 -0700

    Scrolling disabled on map

--- client/src/components/Map/DiveMap.jsx
+++ client/src/components/Map/DiveMap.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import keys from '../../../../keys.js';
 import {withGoogleMap, GoogleMap, Marker, InfoWindow} from 'react-google-maps';
 import MarkerContainer from './MarkerContainer.jsx';
-//import styles from './mapStyles.json';
+// import styles from './mapStyles.json';
 
 const DiveMap = withGoogleMap(props => (
   <GoogleMap
-  //defaultOptions={{styles: styles}}
+  // defaultOptions={{styles: styles}}
    mapTypeId={google.maps.MapTypeId.SATELLITE}
    ref={props.onMapLoad}
    defaultZoom={6}
-
+   defaultOptions={{scrollwheel: false}}
    defaultCenter={{lat: 37.787, lng: -122.458}}
-   onClick={props.onMapClick}
   >
   {props.markers.map( (marker, index) => {
     const formattedMarker = marker;
