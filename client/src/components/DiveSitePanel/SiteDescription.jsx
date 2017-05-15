873f93bc84aa2a8642bcd7756b6f50b6feec325f
jiaconis <john.t.iaconis@gmail.com>
2017-05-15 10:40:43 -0700

    Last(?) styling edits

--- client/src/components/DiveSitePanel/SiteDescription.jsx
+++ client/src/components/DiveSitePanel/SiteDescription.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SiteDescription = (props) => {
 	return (
 	  <div className='col-md-12 weather-section panel-text'>
-	    <h3>About {props.description.name}</h3>
+	    <h3 className="about">About {props.description.name}</h3>
 	    <p>{props.description.description}</p>
     </div>
 	);
